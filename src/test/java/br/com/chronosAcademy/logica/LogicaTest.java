d60aae6a0e3a3cdb6f5fabd64ce067155ac839ca
hpessolato <beto0403>
2021-10-04 17:20:07 -0300

    Aula 03 - Lógica e Testes Unitários

---
+++ src/test/java/br/com/chronosAcademy/logica/LogicaTest.java
@@ -0,0 +1,24 @@
+package br.com.chronosAcademy.logica;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicaTest {
+    @Test
+    @DisplayName("Terreno valor 101 resultado 'Terreno grande'")
+    public void validaTerrenoGrande(){
+        Logica logica = new Logica();
+
+        assertEquals("Terreno grande", logica.getTipoTerreno(101));
+    }
+
+    @Test
+    @DisplayName("Terreno valor 100 resultado 'Terreno pequeno'")
+    public void validaTerrenoPequeno(){
+        Logica logica = new Logica();
+
+        assertEquals("Terreno pequeno", logica.getTipoTerreno(100));
+    }
+}
